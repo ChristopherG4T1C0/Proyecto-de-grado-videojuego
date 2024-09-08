b49a4ea6c5286b008ec13aef836ec6a27aa4c3d9
Christopher Salas <chrisaandrey09@gmail.com>
2024-09-07 20:41:17 -0500

    terminacion de la primera escena completa

--- gato_persona_1.gd
+++ gato_persona_1.gd
@@ -7,7 +7,7 @@ func _ready() -> void:
 
 func _physics_process(delta):
 	var direccion = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
-	velocity =direccion* 200
+	velocity =direccion* 300
 	move_and_slide()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
